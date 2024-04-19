316f7ef6a05d9efbb09bca76ba7c738b8b3aeaff
kellyxc28 <kellyxiongc@gmail.com>
2024-04-18 22:06:37 -0400

    Completed Question 18: swapAllCases()

--- src/modify.js
+++ src/modify.js
@@ -1,12 +1,6 @@
 const swapAllCases = (str) => {
-  let result = '';
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    const isUppercaseLetter = char === char.toUpperCase();
-    result += isUppercaseLetter ? char.toLowerCase() : char.toUpperCase();
-  }
-
-  return result;
+  const swapCase = (char) =>  (/[a-z]/g).test(char) ? char.toUpperCase() : char.toLowerCase();
+  return str.replace((/[A-Za-z]/g), swapCase);
 };
 
 module.exports = {
