ba8c04ebd90c5fc5aeb0df26eab378f9fc3f84ff
kellyxc28 <kellyxiongc@gmail.com>
2024-04-18 21:00:24 -0400

    Completed Question 17: isValidCompanyUsername()

--- src/debug.js
+++ src/debug.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars, no-useless-escape */
 
 const isValidCompanyUsername = (username, firstName, lastName) => {
-  const regex = new RegExp(`^(sales|tech)\d\w-${firstName[0]}${lastName}\d?$`);
+  // the symbol \ is doubled because it is a special symbol, with it being doubled, the \ gets recognized 
+  const regex = new RegExp(`^(sales|tech)\\d\\w-${firstName[0]}${lastName}\\d?$`);
   // console.log(regex) // to help you debug, look at the final regex
   return regex.test(username);
 };
